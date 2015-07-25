d501b367ab8758de4d69af4a6c10e725a058156b
Rachel Xu <rachel960122@hotmail.com>
2015-07-25 16:10:42 +0800

    Add Confirmable to Devise and configure Devise mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   has_many :professors
   has_many :reviews, dependent: :destroy
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
